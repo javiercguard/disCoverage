932d88add296be9317d2e7508e271a5e25ecb462
Javier C. G <javier.cuencag@um.es>
2023-02-15 23:19:59 +0100

    Fixed build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,5 @@
 echo "Installing disCoverage"
+cp disCoverage "$CONDA_PREFIX"/bin/
 cp preparePlottingCoverage.py "$CONDA_PREFIX"/lib/python3.8/site-packages
 mkdir -p "$CONDA_PREFIX"/supp
 cp hg38.bed "$CONDA_PREFIX"/supp
